5575b663296cb7ba3d8b9f974546a6391338fbc9
warzen <warzenkamor@gmail.com>
2015-11-24 00:20:29 +0100

    Ajout des musiques
    Premier lot de musiques

---
+++ cpc_nam_music/CfgMusic.hpp
@@ -0,0 +1,67 @@
+class CfgMusic 
+{
+	class cpc_nam_ridevalk
+		{
+		name="Ride of the Valkyries";
+		sound[]={"\cpc_nam_music\data\ridevalk.ogg",1,1};
+		duration = 305;
+		theme = "safe";
+		};
+
+	class cpc_nam_gortozaran
+		{
+		name="Gortoz a Ran";
+		sound[]={"\cpc_nam_music\data\gortozaran.ogg",1,1};
+		duration = 348;
+		theme = "safe";
+		};
+		
+	class cpc_nam_ForWhatItsWorth
+		{
+		name="For What It's Worth";
+		sound[]={"\cpc_nam_music\data\For_What_It_s_Worth.ogg",1,1};
+		duration = 160;
+		theme = "safe";
+		};
+		
+	class cpc_nam_FortunateSon
+		{
+		name="Fortunate Son";
+		sound[]={"\cpc_nam_music\data\Fortunate_Son.ogg",1,1};
+		duration = 139;
+		theme = "safe";
+		};
+		
+	class cpc_nam_OnTheRoadAgain
+		{
+		name="On The Road Again";
+		sound[]={"\cpc_nam_music\data\On_The_Road_Again.ogg",1,1};
+		duration = 297;
+		theme = "safe";
+		};
+		
+	class cpc_nam_PaintItBlack
+		{
+		name="Paint It Black";
+		sound[]={"\cpc_nam_music\data\Paint_it_Black.ogg",1,1};
+		duration = 226;
+		theme = "safe";
+		};
+
+	class cpc_nam_SweetHomeAlabama
+		{
+		name="Sweet Home Alabama";
+		sound[]={"\cpc_nam_music\data\Sweet_Home_Alabama.ogg",1,1};
+		duration = 300;
+		theme = "safe";
+		};
+
+	class cpc_nam_BootsWalking
+		{
+		name="Those Boots Are Made For Walkin'";
+		sound[]={"\cpc_nam_music\data\Those_Boots_Are_Made_for_Walkin.ogg",1,1};
+		duration = 149;
+		theme = "safe";
+		};
+
+};
--- cpc_nam_music/config.cpp
+++ cpc_nam_music/config.cpp
@@ -10,3 +10,5 @@ class CfgPatches
 		requiredAddons[] = {};
 	};
 };
+
+#include "CfgMusic.hpp"
