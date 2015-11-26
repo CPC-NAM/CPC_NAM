0290d5cb7843c32c709e55ec879c062d124c8945
warzen <warzenkamor@gmail.com>
2015-11-26 22:52:48 +0100

    duplication du son de test
    dispo en volume 1, 2 et 4 pour utilisation au sol ou sur un véhicule
    aérien par exemple

--- cpc_nam_sounds/CfgSounds.hpp
+++ cpc_nam_sounds/CfgSounds.hpp
@@ -10,4 +10,20 @@ class CfgSounds
 		titles[] = {1, "*Ride of the Valkyries*"};
 	};
 
+	class ridevalk_sound2
+	{
+		name = "Ridevalk_Sound2";
+		// filename, volume, pitch
+		sound[]={"\cpc_nam_music\data\ridevalk.ogg", 2, 1};  // volume to be tested for loud-speaker usage
+		titles[] = {1, "*Ride of the Valkyries*"};
+	};
+
+	class ridevalk_sound4
+	{
+		name = "Ridevalk_Sound4";
+		// filename, volume, pitch
+		sound[]={"\cpc_nam_music\data\ridevalk.ogg", 4, 1};  // volume to be tested for loud-speaker usage
+		titles[] = {1, ""};
+	};
+
 };
