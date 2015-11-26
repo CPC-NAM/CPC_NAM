724f1b261b8c97d0769403689f883f170b6c0b22
warzen <warzenkamor@gmail.com>
2015-11-26 15:56:59 +0100

    Ajout de cpc_nam_sounds

---
+++ cpc_nam_sounds/CfgSounds.hpp
@@ -0,0 +1,13 @@
+class CfgSounds
+{
+
+	sounds[] = {};
+	class ridevalk_sound
+	{
+		name = "Ridevalk_Sound";
+		// filename, volume, pitch
+		sound[]={"\cpc_nam_music\data\ridevalk.ogg", 1, 1};  // volume to be tested for loud-speaker usage
+		titles[] = {1, "*Ride of the Valkyries*"};
+	};
+
+};
---
+++ cpc_nam_sounds/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+	class cpc_nam_sounds
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"cpc_nam_music"};
+	};
+};
+
+#include "CfgSounds.hpp"
